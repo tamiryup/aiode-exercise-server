ebb716471fd92ed0d406cf43627eb7e6a51d1b01
tamiryup <tamirishaysh@gmail.com>
2023-11-25 15:25:29 +0200

    Create initDatabase config

---
+++ config/initDatabase.js
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const dbName = 'music-library.db';
+
+const db = new sqlite3.Database(dbName, (err) => {
+  if (err) {
+    console.error(`Error opening database ${dbName}: ${err.message}`);
+  } else {
+    console.log(`Connected to the database: ${dbName}`);
+    createTables();
+  }
+});
+
+function createTables() {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS tracks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        artist TEXT NOT NULL,
+        genre TEXT NOT NULL
+    )
+  `, (err) => {
+    if (err) {
+      console.error(`Error creating table: ${err.message}`);
+    } else {
+      console.log('Table created successfully.');
+    }
+
+    db.close();
+  });
+}
